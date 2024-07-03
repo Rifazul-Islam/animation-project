352d23b6a171972181f976dbdf717561014a3dd0
Rifazul-Islam <rifazul50@gmail.com>
2024-07-03 18:00:22 +0600

    add new page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+ 
+
+  return (
+    <>
+     
+ <p className="text-3xl text-blue-600 text-center"> This is A Animation Part </p>
+      <p> Three js Animation Tryed </p>
+    </>
+  )
+}
+
+export default App
