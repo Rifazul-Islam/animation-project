b9e8aa61085e58a3f8d3d0378cb79adf1b987f2b
Rifazul-Islam <rifazul50@gmail.com>
2024-07-13 09:26:59 +0600

    insert animation picture

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ function App() {
      
  <p className="text-3xl text-blue-600 text-center"> This is A Animation Part </p>
       <p> Three js Animation Tryed </p>
+    <div >
+    <img className="w-full  h-[500px]" src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/1e94d557441295.59d5dfd469876.gif" alt="" />
+    </div>
+      {/* <img src="https://payload.cargocollective.com/1/3/125982/11115286/superman-animated-gif.gif" alt="" /> */}
     </>
   )
 }
